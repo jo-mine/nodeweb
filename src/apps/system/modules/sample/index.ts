2f42223bbd93ed3fd8b46caa04ae303dd4fae766
jo-mine <minetab25@gmail.com>
2024-03-29 22:43:49 +0900

    eslint修正中

--- src/apps/system/modules/sample/index.ts
+++
@@ -1,6 +0,0 @@
-import { controller, interfaces } from "inversify-express-utils";
-
-@controller("/system/sample")
-export class controller implements interfaces.Controller {
-    
-}
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,31 @@
 import { log } from 'console'
-import "reflect-metadata"
+import 'reflect-metadata'
 import errorHandler from './libs/middleware/errorHandler'
 import callableHandler from './libs/middleware/callableHandler'
 import { Container } from 'inversify'
 import { InversifyExpressServer } from 'inversify-express-utils'
 
+const port = 8080
 const container = new Container()
 const server = new InversifyExpressServer(container)
-const app = server.build()
-    .set("view engine", "ejs")
-    .all('/index/:app-:module-:action',
-    callableHandler,
-    async (req, res) => {
-        const { app, module, action } = req.params
-        const modulePath = `./apps/${app}/modules/${module}/index.ts`
+server
+  .build()
+  .all('/', (req, res) => {
+    res.end('end')
+  })
+// .set("view engine", "ejs")
+// .all('/index/:app-:module-:action',
+// callableHandler,
+// async (req, res) => {
+//     const { app, module, action } = req.params
+//     const modulePath = `./apps/${app}/modules/${module}/index.ts`
 
-        const serverAction = (await import(modulePath))[action]
-        serverAction()
-        // res.render('<?=text?>', {text: 'hello world'})
-        res.end('end world')
-    })
-    .use(errorHandler) // エラーハンドラは最後に追加する
-
-const port = 8080
-
-app.listen(port)
+//     const serverAction = (await import(modulePath))[action]
+//     serverAction()
+//     // res.render('<?=text?>', {text: 'hello world'})
+//     res.end('end world')
+// })
+// .use(errorHandler) // エラーハンドラは最後に追加する
+  .listen(port)
 
 log(`listening the port: ${port}`)
